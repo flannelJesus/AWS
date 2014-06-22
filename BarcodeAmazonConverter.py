28ac5bcf6c2bdb307f9aea0f27983fabb11ea249
Kremlin <sjoshuaross@yahoo.com>
2014-06-22 19:47:48 +0100

    correctly handles strings returned from amazon if unicode

--- BarcodeAmazonConverter.py
+++ BarcodeAmazonConverter.py
@@ -1,5 +1,13 @@
 import amazonproduct
 from BeautifulSoup import asin2min, asin2bn
+import unicodedata
+
+def if_error(func, input, default_value):
+    try:
+        r = func(input)
+    except:
+        r = default_value
+    return r
 
 api = amazonproduct.API(locale='uk')
 
@@ -24,26 +32,34 @@ for line in barcodes:
             #except:
             #    output.write("%s|exception||||\n" % (bc))
 
-            try:
-                asin = str(item.ASIN)
-            except:
-                asin = ""
-            try:
-                title = str(item.ItemAttributes.Title)
-            except:
-                title = ""
-            try:
-                rank = str(item.SalesRank)
-            except:
-                rank = ""
-            try:
-                min = asin2min(item.ASIN)
-            except:
-                min = ""
-            try:
-                bn = asin2bn(item.ASIN)
-            except:
-                bn = ""
+            #try:
+            #    asin = str(item.ASIN)
+            #except:
+            #    asin = ""
+            #try:
+            #    title = str(item.ItemAttributes.Title)
+            #except:
+            #    title = ""
+            #try:
+            #    rank = str(item.SalesRank)
+            #except:
+            #    rank = ""
+            #try:
+            #    min = asin2min(item.ASIN)
+            #except:
+            #    min = ""
+            #try:
+            #    bn = asin2bn(item.ASIN)
+            #except:
+            #    bn = ""
+
+            if bc == "5060149520178":
+                pass
+            asin = str(item.ASIN) if hasattr(item, "ASIN") else ""
+            title = str(item.ItemAttributes.Title.text.encode('ascii','ignore')) if hasattr(item, "ItemAttributes") and hasattr(item.ItemAttributes, "Title") else ""
+            rank = str(item.SalesRank) if hasattr(item, "SalesRank") else ""
+            min = asin2min(item.ASIN) if hasattr(item, "ASIN") else ""
+            bn = asin2bn(item.ASIN) if hasattr(item, "ASIN") else ""
             output.write('%s|%s|%s|%s|%s|%s\n' % (bc, asin, title, rank, min, bn))
 
     except:
--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -3,24 +3,27 @@ import urllib2
 
 
 def asin2min(asin):
-    x = urllib2.urlopen(
-        "http://www.amazon.co.uk/gp/offer-listing/" + asin + "/ref="
-        "dp_olp_new_mbc?ie=UTF8&condition=new").read()
+    try:
+        x = urllib2.urlopen(
+            "http://www.amazon.co.uk/gp/offer-listing/" + asin + "/ref="
+            "dp_olp_new_mbc?ie=UTF8&condition=new").read()
 
-    soup = BeautifulSoup(x, "html.parser")
-    pricet = soup.find(class_="olpOfferPrice")
-    if pricet != None:
-        price = float(pricet.text.strip()[1:])
-        shippingp = 0
-        for sibling in soup.find(class_="olpOfferPrice").next_siblings:
-            sibling = str(sibling)
-            if 'class="olpShippingPrice"' in sibling:
-                ship = BeautifulSoup(sibling)
-                shipping = ship.find(class_="olpShippingPrice")
-                shippingp = float(shipping.text.strip()[1:])
+        soup = BeautifulSoup(x, "html.parser")
+        pricet = soup.find(class_="olpOfferPrice")
+        if pricet != None:
+            price = float(pricet.text.strip()[1:])
+            shippingp = 0
+            for sibling in soup.find(class_="olpOfferPrice").next_siblings:
+                sibling = str(sibling)
+                if 'class="olpShippingPrice"' in sibling:
+                    ship = BeautifulSoup(sibling)
+                    shipping = ship.find(class_="olpShippingPrice")
+                    shippingp = float(shipping.text.strip()[1:])
 
-        return str(shippingp + price)
-    else:
+            return str(shippingp + price)
+        else:
+            return ""
+    except:
         return ""
 
 def asin2bn(asin):
