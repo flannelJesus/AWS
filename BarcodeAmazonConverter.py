1c50c7c1ab8b03150b8c2daf499c3d023e97d891
flannel jesus <sjoshuaross@yahoo.com>
2016-12-16 16:50:11 +0000

    minor updates

--- BarcodeAmazonConverter.py
+++ BarcodeAmazonConverter.py
@@ -1,6 +1,7 @@
 import amazonproduct
 from BeautifulSoup import asin2min, asin2bn
 import unicodedata
+import time
 
 
 api = amazonproduct.API(locale='uk')
@@ -8,7 +9,7 @@ api = amazonproduct.API(locale='uk')
 
 barcodes = open('barcodes.txt')
 output = open('b2a.csv', 'w')
-output.write('sep = |\nbarcode|ASIN|Amazon Title|Rank|MinPrice|BuyNow\n')
+output.write('sep = |\nbarcode|ASIN|Amazon Title|Rank|MinPrice\n')
 x = 0
 y = 1
 for line in barcodes:
@@ -16,10 +17,8 @@ for line in barcodes:
     bc = line.strip()
     try:
         result = api.item_lookup(bc, SearchIndex='All', IdType='EAN',
-                                 ResponseGroup="Small,SalesRank")
+                                 ResponseGroup="Small,SalesRank,Offers")
         for item in result.Items.Item:
-            if bc == "5060149520178":
-                pass
             asin = str(item.ASIN) if hasattr(item, "ASIN") else ""
             title = str(
                 item.ItemAttributes.Title.text.encode(
@@ -27,9 +26,14 @@ for line in barcodes:
                 hasattr(item, "ItemAttributes") and \
                 hasattr(item.ItemAttributes, "Title") else ""
             rank = str(item.SalesRank) if hasattr(item, "SalesRank") else ""
-            minprice = asin2min(item.ASIN) if hasattr(item, "ASIN") else ""
-            bn = asin2bn(item.ASIN.text) if hasattr(item, "ASIN") else ""
-            output.write('%s|%s|%s|%s|%s|%s\n' % (bc, asin, title, rank, minprice, bn))
-    except:
-        output.write("%s|exception||||\n" % (bc))
+            # minprice = asin2min(item.ASIN) if hasattr(item, "ASIN") else ""
+            minprice = item.OfferSummary.LowestNewPrice.Amount if hasattr(item.OfferSummary, "LowestNewPrice") else ""
+            # bn = asin2bn(item.ASIN.text) if hasattr(item, "ASIN") else ""
+            # bn = 0
+            output.write('%s|%s|%s|%s|%s\n' % (bc, asin, title, rank, minprice))
+    except Exception as err:
+        print err
+        output.write("%s|exception|%s||\n" % (bc, err.msg))
+    time.sleep(1)
+
 output.close()
